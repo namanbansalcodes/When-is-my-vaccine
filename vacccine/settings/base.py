62f658a08fad2936425248b5d5cfce82aed97bce
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-02 22:50:34 +0530

    allowed hosts fix

--- vacccine/settings/base.py
+++ vacccine/settings/base.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
