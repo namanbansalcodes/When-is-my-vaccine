7b682cce1d17b2092abe6e26afbb78d1501f9ddb
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-02 20:34:24 +0530

    Following docu

--- vacccine/settings/base.py
+++ vacccine/settings/base.py
@@ -40,11 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
