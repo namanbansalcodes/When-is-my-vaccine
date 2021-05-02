054b9f8e1b4910798af4247f18f7db328a11ebdf
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-02 22:59:03 +0530

    fixed allowed hosts error

--- vacccine/settings/heroku.py
+++ vacccine/settings/heroku.py
@@ -5,13 +5,12 @@ Production Settings for Heroku
 import environ
 
 # If using in your own project, update the project namespace below
-from vacccine.settings.base import *
+from gettingstarted.settings.base import *
 
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
 
 # False if not in os.environ
 DEBUG = env('DEBUG')
@@ -19,7 +18,7 @@ DEBUG = env('DEBUG')
 # Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
