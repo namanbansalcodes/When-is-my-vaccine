ecbbbe76fe5fd053280e597408905079019101c8
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-03 18:41:27 +0530

    phone no as string

--- home/forms.py
+++ home/forms.py
@@ -3,5 +3,5 @@ from .models import Customer
 
 
 class CustomerForm(forms.Form):
-    phone = forms.CharField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    phone = forms.CharField(required=True, strip=True, max_length=14, widget=forms.TextInput(attrs={'class' : 'form-control'}))
     pin = forms.IntegerField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control'}))
--- home/models.py
+++ home/models.py
@@ -6,8 +6,7 @@ phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number mus
 
 # Create your models here.
 class Customer(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone = models.CharField(validators=[phone_regex], max_length=13, blank=True) # validators should be a list
+    phone = models.CharField(validators=[phone_regex],max_length=14)
     pin = models.IntegerField()
 
 
