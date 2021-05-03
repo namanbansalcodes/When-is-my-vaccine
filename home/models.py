41511201843712c10c5022c812f759f5b4023687
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-03 18:53:53 +0530

    phone validator

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ import uuid
 # Create your models here.
 class Customer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone = models.CharField()
+    phone = models.CharField(max_length=14)
     pin = models.IntegerField()
 
 
