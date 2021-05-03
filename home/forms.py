fe2558c0d3ab339c3fa2830a2475047316293e57
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-03 18:30:30 +0530

    finally fixed

--- home/forms.py
+++ home/forms.py
@@ -3,5 +3,5 @@ from .models import Customer
 
 
 class CustomerForm(forms.Form):
-    phone = forms.RegexField(regex=r'^\+?1?\d{9,15}$', widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    phone = forms.CharField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control'}))
     pin = forms.IntegerField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control'}))
