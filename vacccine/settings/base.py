585f03821f7bdeb2ebf09afd9dfa5244d44d538c
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-03 14:39:52 +0530

    removed dirs from templates

--- vacccine/settings/base.py
+++ vacccine/settings/base.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'vacccine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
