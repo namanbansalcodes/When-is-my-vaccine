30dd20cf9fbbcfa5986d13e92e637f65c94fdc3a
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-03 17:56:30 +0530

    added phone field

--- home/forms.py
+++ home/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from .models import Customer
 
 class CustomerForm(forms.Form):
-    phone = forms.IntegerField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    phone = forms.PhoneNumberField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control'}))
     pin = forms.IntegerField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control'}))
--- home/models.py
+++ home/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 import uuid
+from phone_field import PhoneField
 
 # Create your models here.
 class Customer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone = models.IntegerField()
+    phone = PhoneField(blank=False)
     pin = models.IntegerField()
 
 
