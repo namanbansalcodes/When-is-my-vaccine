c3b21494fc04f143560d63821c254fe48b08854f
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-03 14:45:00 +0530

    adding import templates in views

--- vacccine/settings/base.py
+++ vacccine/settings/base.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'vacccine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
