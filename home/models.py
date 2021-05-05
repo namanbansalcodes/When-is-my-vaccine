26ce2e778f810ee814e485bab7bdb7210f424dab
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-05 15:14:33 +0530

    added defaults

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ import uuid
 # Create your models here.
 class Customer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone = models.CharField(max_length=14)
+    phone = models.CharField(max_length=10,default='123456789')
     email = models.CharField(max_length=50,default='xuz@gmail.com')
     pin = models.IntegerField()
     flag1 = models.IntegerField(default=0)
