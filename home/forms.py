a596469083bc1180bc81b36de30f75f6e5bb32d8
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-06 02:20:43 +0530

    slight visual changes

--- home/forms.py
+++ home/forms.py
@@ -4,16 +4,15 @@ from .models import Customer
 
 class CustomerForm(forms.Form):
     email = forms.CharField(required=True, strip=True,
-                        widget=forms.TextInput(attrs={'class': 'form-control'}), label='Email',
-                        label_suffix=''
-                        )
+                            widget=forms.TextInput(attrs={'class': 'form-control'}), label='Email',
+                            label_suffix=''
+                            )
 
     phone = forms.CharField(required=True, strip=True, max_length=10,
                             widget=forms.TextInput(attrs={'class': 'form-control'}), label='Phone Number',
                             label_suffix=''
                             )
 
-    pin = forms.CharField(required=True, max_length= 60,
-                             widget=forms.TextInput(attrs={'class': 'form-control'}), label_suffix='')
-
-
+    pin = forms.CharField(required=True, max_length=60,
+                          widget=forms.TextInput(attrs={'class': 'form-control'}), label='Pincodes (seperated by space)', 
+                          label_suffix='')
