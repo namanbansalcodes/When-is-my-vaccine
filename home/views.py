83a6039e01c63e9c4c7a42928f154772e55ddbcf
Naman Bansal <namanbansalcodes@gmail.com>
2021-05-05 16:45:38 +0530

    Release 1

--- home/views.py
+++ home/views.py
@@ -14,7 +14,7 @@ def index(request):
         phone = request.POST['phone'].lower()
 
         if len(phone) == 10 and len(pin) == 6:
-            if re.match(r"[^@]+@[^@]+\.[^@]+", email) and (len(re.findall('[a-z]+', pin)) == 0 and len(re.findall('[a-z]+', phone))==0 ) :
+            if re.match(r"[^@]+@[^@]+\.[^@]+", email) and (len(re.findall('[a-z]+', pin)) == 0 and len(re.findall('[a-z]+', phone)) == 0):
 
                 if len(Customer.objects.filter(pin=pin)) != 0:
                     flag2 = Customer.objects.filter(pin=pin)[0].flag2
@@ -30,6 +30,14 @@ def index(request):
                 )
 
                 user.save()
+
+                requests.post(
+                    "https://api.mailgun.net/v3/sandbox8d2f3b59dbeb4627bc07e28a3284f960.mailgun.org/messages",
+                    auth=("api", "83c8481eed353ca9d76bbdd3101a2b33-2a9a428a-5bb25d17"),
+                    data={"from": "When is my vaccine? <alerts@whenismyvaccine.in>",
+                          "to": email,
+                          "subject": "Thank for registering to WhenIsMyVaccine",
+                          "text": f"Hello {email},\nThank you for registering to WhenISMyVaccine!\n\nWe have reveived your request to receive alerts whenever vaccines get restocked in you area!\n You have registerd for the alerts for the pin: {pin}"})
 
                 return render(request, './done.html')
 
